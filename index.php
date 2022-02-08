9f24f8d06462ff0cc306f61da0d499acbc75158e
pmedia84 <80710345+pmedia84@users.noreply.github.com>
2022-02-08 15:28:28 +0000

    landing page

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php include("inc/nav.inc.php"); ?>
+        <title>Well Being From Within - Coming Soon</title>
+</head>
+
+<body>
+    <header>
+        <nav class="navcontainer">
+            <div class="navbar">
+                <!--<p class="navbar-link">Contact Me</p> -->
+            <div class="navbar-brand">
+                <img src="img/logo.svg" alt="Well Being From Within Logo">
+            </div>
+         <!--   <div class="navbar-menu-btn">
+                <p class="menu-text">Menu</p>
+                <div class="menu-bars">
+                    <div class="menu-bar"></div>
+                    <div class="menu-bar"></div>
+                </div>-->
+            </div>
+            </div>
+        </nav>
+    </header>
+
+<main>
+    <div class="">
+        <div class="hero">
+            <p class="herotitle">Mental Health Support And Reiki Services</p>
+            <p>My website is currently under development</p>
+            <p>For any enquiries, please contact me on the information below:</p>
+            <p>Mobile: 07769 211532</p>
+            <p>melissa@well-beingfromwithin.co.uk</p>
+            <img class="hero-image" src="img/melissadye.png" alt="Image of Melissa Dye">
+        </div>
+    </div>
+</main>
+
+</body>
+
+</html>
